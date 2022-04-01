3387c8a346ee84731ba35cd45a9733a6f6208719
Guilherme Denck <guidenck0303@gmail.com>
2022-04-01 18:40:46 -0300

    feat/adicionando svg para header e home

--- src/pages/Home/Home.screen.jsx
+++ src/pages/Home/Home.screen.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import style from './Home.module.css';
+import { useEffect, useContext } from "react";
+import { NewYorkContext } from '../../context/NewYork.context';
+import Logo from '../../images/logo.svg'
+import titulo from '../../images/nytimes-wordmark.svg'
 
+import style from './Home.module.css';
 const Home = () => {
+
+  const { getNewsHome, home } = useContext(NewYorkContext);
+
+  console.log(home);
+
+  useEffect(() => {
+    getNewsHome();
+  }, [])
+
   return (
-    <div className={style.home}>Home</div>
+    <div className={style.home}>
+      <h1>Home</h1>
+      <img src={Logo} alt="logo" />
+      <img src={titulo} alt="titulo" />
+    </div>
   )
 }
 
