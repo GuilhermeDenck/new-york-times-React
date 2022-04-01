dfbc2ce9c3a2755ae997662111a92fb2133165a9
Guilherme Denck <guidenck0303@gmail.com>
2022-04-01 17:41:07 -0300

    initial commit

---
+++ src/context/NewYork.context.jsx
@@ -0,0 +1,83 @@
+import { createContext, useState } from "react";
+import api from "../services/api";
+
+export const NewYorkContext = createContext();
+
+const NewYorkProvider = ({children}) => {
+
+  const [home, setHome] = useState([])
+  const [world, setWorld] = useState([]);
+  const [food, setFood] = useState([]);
+  const [business, setBusiness] = useState([]);
+  const [tech, setTech] = useState([]);
+
+  const formatNews = (news) => {
+    const newsFormated = news.results.map( e => {
+      return [
+        e.title,
+        e.abstract,
+        e.url,
+        e.multimedia,
+        e.published_date
+      ]
+    });
+
+    console.log(newsFormated);
+    return news;
+  }
+
+  const getNewsHome = async () => {
+    try {
+      const {data} = await api.get('/home.json?api-key=jW6a1bsrsqzbYBm8WhnM72WRvvVks4OM');
+      setHome(formatNews(data));
+      console.log(home);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getNewsWorld = async () => {
+    try {
+      const {data} = await api.get('/world.json?api-key=jW6a1bsrsqzbYBm8WhnM72WRvvVks4OM');
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getNewsFood = async () => {
+    try {
+      const {data} = await api.get('/food.json?api-key=jW6a1bsrsqzbYBm8WhnM72WRvvVks4OM');
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getNewsBusiness = async () => {
+    try {
+      const {data} = await api.get('/business.json?api-key=jW6a1bsrsqzbYBm8WhnM72WRvvVks4OM');
+      return data;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getNewsTech = async () => {
+    try {
+      const {data} = await api.get('/technology.json?api-key=jW6a1bsrsqzbYBm8WhnM72WRvvVks4OM');
+      return data;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <NewYorkContext.Provider value={{ getNewsHome, home }}>
+      {children}
+    </NewYorkContext.Provider>
+  );
+}
+
+export default NewYorkProvider;
