e00ab5ad885be6988d05c1d0dcda688a741f0248
Guilherme Denck <guidenck0303@gmail.com>
2022-04-01 17:51:12 -0300

    fix/ajustando app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NewYorkProvider from './context/NewYork.context'
+
+import Home from './pages/Home/Home.screen';
+import SectionsNews from './pages/SectionsNews/SectionsNews.screen';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <NewYorkProvider>
+        <Routes>
+          <Route path='*' element={<Home />}/>
+          <Route exact path='/nyttop' element={<Home />}/>
+          <Route path='/section' element={<SectionsNews />}> 
+            <Route path=':typeNews' element={<SectionsNews/>}/>
+          </Route>
+        </Routes>
+      </NewYorkProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/SectionsNews/SectionsNews.screen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SectionsNews = () => {
+  return (
+    <div>SectionsNews</div>
+  )
+}
+
+export default SectionsNews;
